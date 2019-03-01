6e615f6bf0c647c45ba9ad56cdc3f0dda8b42c0b
A Belgrave <abelgrave95@gmail.com>
2019-03-01 01:26:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,9 @@ def nyc_pigeon_organizer(data)
   result_hash = {}
   data.each { |atr, info| info.each do |type, name|
     if !result_hash[name]
-      puts name
+      # puts name
       result_hash[name]= {atr => [type.to_s]}
+      puts result_hash
     else
       result_hash[name][atr] << type
     end
